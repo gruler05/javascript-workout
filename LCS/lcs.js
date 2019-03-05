c31e805fa985815194145dceb14d2b6415d5f1f5
Gaurav Bansode <bansodegaurav@gmail.com>
2019-03-05 17:28:47 -0600

    adding index.js to avoid error on default in codesandbox

--- LCS/lcs.js
+++ LCS/lcs.js
@@ -3,7 +3,7 @@ const LCS = (str1, str2) => {
   const arr2 = [...str2];
 
   let start2 = 0;
-  let str = "";
+  let str = '';
   for (let i = 0; i < arr1.length; i++) {
     const char1 = arr1[i];
     for (let j = start2; j < arr2.length; j++) {
@@ -17,15 +17,15 @@ const LCS = (str1, str2) => {
   // console.log(str)
   return str;
 };
-const str1 = "ABCDGH";
+const str1 = 'ABCDGH';
 const str2 = [...str1];
 const test = [];
 for (let i = 0; i < 6; i++) {
   if (i > 0) {
     str2.shift();
   }
-  const str = i > 0 ? str2.join("") : str1;
-  test.push(LCS(str, "AEDFHR"));
+  const str = i > 0 ? str2.join('') : str1;
+  test.push(LCS(str, 'AEDFHR'));
 }
 
 console.log(
